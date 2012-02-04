b567372aef16dc14521f6e54e810c87fbf24ef69
Lukas <lpeyer@gmail.com>
2012-02-04 17:33:09 +0100

    new rails 3.2.1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PublicspaceRails::Application.config.secret_token = '8fc16b702d0e592aabd514ac41bdba2d7203509bf4f6d18fe2751a157def2f16f0e09bf1afbc09ea11d0789719beb9dd16d0586ee632103b24fda51bd4e8990a'
